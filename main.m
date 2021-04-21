9cab26deeeb1e7662311495656be13a9f6eeb9ae
Hardi-Mohammed <48719027+Hardi-Mohammed@users.noreply.github.com>
2021-04-21 09:39:15 +0300

    Update main.m

--- main.m
+++ main.m
@@ -1,9 +1,10 @@
 % %
-%% RFSO 
+%% RFSO
 % Red Fox in the Snow Optimization Algorithm for Solving Pressure Vessel
 % Desing Problem.
 % Authors: Hardi M. Mohammed, Tarik A. Rashid.
 % RFSO is implemented by Hardi M. Mohammed
+%Review three #R3
 %%
 clear all 
 clc
@@ -11,8 +12,8 @@ summ=0;
 avg=0;
  for i=1:30
 SearchAgents_no=30; % Number of search agents
-
-Function_name='cec01'; % Name of the test function that can be from 1 to 10
+%30, cec01, max it=100
+Function_name='cec09'; % Name of the test function that can be from 1 to 10
 
 Max_iteration=500; % Maximum numbef of iterations
 
@@ -28,3 +29,4 @@ display( num2str(Best_score));
  end
  avg=summ/30;
  [avg]
+
